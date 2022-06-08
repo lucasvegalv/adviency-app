e128da00b55d16ab503f36eb1531cc1ea821de2c
Lucas <lucasvega02@gmail.com>
2022-06-07 23:59:59 -0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -19,26 +19,30 @@ const handleChange = (e) => {
   setValue(e.target.value)
   }
   
-  // ADD INPUT TO THE LIST
-  const handleClick = () => {
-    
-  const item = {
-    id: uniqueId,
-    value: value,
-    completed: false
-  } 
-
-  if(value != '') {
-    setItems([...items, item])
-  } else {
-    alert("Please enter a present")
+  // SELECT ITEM QUANTITY
+  const handleSelect = (e) => {
+    const itemQuant = e.target.value
+    console.log(itemQuant)
   }
   
-  setValue('')
+  // ADD INPUT TO THE LIST
+  const handleAdd = () => {
+    const item = {
+      id: uniqueId,
+      value: value,
+      completed: false
+    } 
+
+    if(value != '') {
+        setItems([...items, item])
+    } else {
+      alert("Please enter a present")
+    }
+    setValue('')
   }
 
   // ADD THE INPUT PRESSING 'ENTER' KEY 
-  const handleKeyDown = (e) => { e.key === 'Enter' && handleClick() }
+  const handleKeyDown = (e) => { e.key === 'Enter' && handleAdd() }
   
   // DELETE ITEM
   const handleDelete = (id) => {
@@ -52,10 +56,13 @@ const handleChange = (e) => {
     const deletedAll = items.filter(item => item != item)
     setItems(deletedAll)
   }
+
+
  
   // RENDER THE ITEM LIST
-  const itemList = items.map(item => (<li key={item.id} >{item.value} <button onClick={() => handleDelete(item.id)}>X</button></li>))
+  const itemList = items.map(item => (<li key={item.id}> {item.value} <button onClick={() => handleDelete(item.id)}>X</button></li>))
 
+  
   // ALERT THE USER IF THERE'S NO ITEMS YET
   const noItemsAlert = items.length === 0 && <p>Christmas is coming! Let's buy some cool things</p>
 
@@ -68,7 +75,19 @@ const handleChange = (e) => {
 
       <div>
         <input type="text" onKeyDown={handleKeyDown} onChange={handleChange} value={value} placeholder="Add a present to buy" autoFocus/>
-        <button onClick={handleClick}>Add</button>
+        <select name="selectNumbers" id="selectNumbers" onChange={handleSelect}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+        <button onClick={handleAdd}>Add</button>
         <ul>
           {itemList}
           {noItemsAlert}
