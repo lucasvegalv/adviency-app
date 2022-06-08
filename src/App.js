a4be24f5296fe22bc2eb45fd5301207579572d0f
Lucas <lucasvega02@gmail.com>
2022-06-08 17:26:05 -0300

    Select Item Quantity's funcitonality added

--- src/App.js
+++ src/App.js
@@ -6,64 +6,66 @@ function App() {
 // HOOKS 
 const [ value, setValue ] = useState('')
 const [ items, setItems ] = useState([])
+const [ quant, setQuant ] = useState('')
 
-// GENERATE UNIVERSAL UNIQUE ID
+// Generate Universal Unique IDs
 const uniqueId = uuid()
-
-// ITEM OBJ
 
 // EVENT HANDLER FUNCTIONS
 
-// SAVE INPUT CHANGES
+// Save Input Changes
 const handleChange = (e) => {
   setValue(e.target.value)
   }
   
-  // SELECT ITEM QUANTITY
-  const handleSelect = (e) => {
-    const itemQuant = e.target.value
-    console.log(itemQuant)
-  }
   
-  // ADD INPUT TO THE LIST
+  // Add Input To The List
   const handleAdd = () => {
     const item = {
       id: uniqueId,
       value: value,
+      quant: quant,
       completed: false
     } 
 
-    if(value != '') {
-        setItems([...items, item])
+    if(value !== '') {
+      setItems([...items, item])
     } else {
       alert("Please enter a present")
     }
     setValue('')
   }
 
-  // ADD THE INPUT PRESSING 'ENTER' KEY 
+  // Add the input pressing 'enter' key
   const handleKeyDown = (e) => { e.key === 'Enter' && handleAdd() }
   
-  // DELETE ITEM
+  // Delete Item
   const handleDelete = (id) => {
-    const filteredItems = items.filter(items => items.id != id)
+    const filteredItems = items.filter(items => items.id !== id)
     setItems(filteredItems)
-    console.log(items)
   }
   
-  // DELETE ALL ITEMS
+  // Delete All Items
   const handleDeleteAllItems = () => {
-    const deletedAll = items.filter(item => item != item)
+    const deletedAll = items.filter(item => item !== item)
     setItems(deletedAll)
   }
-
-
- 
-  // RENDER THE ITEM LIST
-  const itemList = items.map(item => (<li key={item.id}> {item.value} <button onClick={() => handleDelete(item.id)}>X</button></li>))
-
   
-  // ALERT THE USER IF THERE'S NO ITEMS YET
+  // Select Item Quantity
+  const handleSelectQuant = (e) => {
+    const itemQuant = e.target.value
+    setQuant(itemQuant)
+    console.log(quant)
+  }
+
+  // Render The Item's List
+  const itemList = items.map(item => 
+    (
+      <li key={item.id}> {item.value} {(item.quant) > 1 && `(${item.quant})`} <button onClick={() => handleDelete(item.id)}>X</button> </li>
+    ))
+  
+  // Conditional Item Quant Render  
+  // Alert the user if there's no items yet
   const noItemsAlert = items.length === 0 && <p>Christmas is coming! Let's buy some cool things</p>
 
   return (
@@ -75,7 +77,7 @@ const handleChange = (e) => {
 
       <div>
         <input type="text" onKeyDown={handleKeyDown} onChange={handleChange} value={value} placeholder="Add a present to buy" autoFocus/>
-        <select name="selectNumbers" id="selectNumbers" onChange={handleSelect}>
+        <select name="selectNumbers" id="selectNumbers" onChange={handleSelectQuant}>
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
